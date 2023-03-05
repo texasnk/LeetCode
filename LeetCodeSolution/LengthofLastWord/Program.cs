93d6b5732fe9cfbbbd364f5b40962682585b5d5f
texasnk <davirodriguesnk@gmail.com>
2023-03-05 20:12:21 -0300

    feat: Add Lengh Of Last Word solution

---
+++ LeetCodeSolution/LengthofLastWord/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LengthofLastWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(LengthOfLastWord("Hello, world! "));
+
+            Console.ReadKey();
+        }
+        public static int LengthOfLastWord(string s)
+        {
+            s = s.TrimEnd(' ');
+            Console.WriteLine(s.Remove(0,s.LastIndexOf(' ')+1).Count());
+
+            return 1;
+        }
+    }
+}
