5fead3e831ae4749645d7cc34ce5047e3c9e4d19
texasnk <davirodriguesnk@gmail.com>
2023-03-05 15:57:12 -0300

    feat: Add Search Insert Position solution

---
+++ LeetCodeSolution/SearchInsertPosition/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SearchInsertPosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] nums = { 1, 3, 5, 6 };
+            int res = SearchInsert(nums, 5);
+
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+        public static int SearchInsert(int[] nums, int target)
+        {
+            int k=0;
+            if (nums.Length == 0) return 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < target) k++;
+            }
+
+            return k;
+        }
+    }
+}
