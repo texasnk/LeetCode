88778a5d50526cfcfe5da922870c990958653472
texasnk <davirodriguesnk@gmail.com>
2023-03-05 11:02:40 -0300

    feat: Add Merge Two Sorted Lists solution

---
+++ LeetCodeSolution/LeetCode/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MergeTwoSortedLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Solution a = new Solution();
+            ListNode list1 = new ListNode(1);
+            list1.next = new ListNode(2);
+            list1.next.next = new ListNode(4);
+            ListNode list2 = new ListNode(1);
+            list2.next = new ListNode(3);
+            list2.next.next = new ListNode(4);
+
+            var res = a.MergeTwoLists(list1, list2);
+           
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LeetCodeSolution/LeetCode/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeTwoSortedLists
+{
+    public class Solution
+    {
+
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+ 
+            if (list1 is null) return list2;
+            else if (list2 is null) return list1;
+
+            ListNode dummy = new ListNode(), point = dummy;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val < list2.val)
+                {
+                    point.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    point.next = list2;
+                    list2 = list2.next;
+                }
+
+                point = point.next;
+            }
+
+            if (list1 != null)
+                point.next = list1;
+
+            if (list2 != null)
+                point.next = list2;
+
+            return dummy.next;
+        }
+
+    }
+}
