1f0b35ba911878707ff40867ac8bc7415510248a
texasnk <davirodriguesnk@gmail.com>
2023-03-06 15:42:18 -0300

    feat: Add SquareRoot solution

---
+++ LeetCodeSolution/SquareRoot/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(MySqrt(17785));
+
+            Console.ReadKey();
+        }
+        public static int MySqrt(int x)
+        {
+            if (x == 0)
+                return x;
+            int first = 1, last = x;
+            while (first <= last)
+            {
+                int mid = first + (last - first) / 2;
+
+                if (mid == x / mid)
+                    return mid;
+                else if (mid > x / mid)
+                {
+                    last = mid - 1;
+                }
+                else
+                {
+                    first = mid + 1;
+                }
+            }
+            return last;
+        }
+    }
+}
